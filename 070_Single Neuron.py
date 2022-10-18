7d969d5228b1662c9b9b1ceac5d5304017165262
widyasw <99782577+widyasw@users.noreply.github.com>
2022-10-18 23:19:04 +0700

    Update 070_Single Neuron.py

--- 070_Single Neuron.py
+++ 070_Single Neuron.py
@@ -12,7 +12,7 @@ inputs = [7, 2, 5.3, 4.5, 3, 6, 10, 8, 1, 9]
 weights = [6.4, 2.8, 0.5, 5.3, 2.7, -2.5, 0.10, 5.6, 3.2, 1.8]
 
 # inisialisasi bias
-# jumlah bias sama dengan jumlah neuron, yaitu 1
+# jumlah bias dan jumlah neuron sama, yaitu 1
 bias = 4.0
 
 # penghitungan output
